a31512249355813f04b7434dfa0b51cd2c8e72ea
brunacostaz <brunacstcandeias@gmail.com>
2025-03-23 17:55:49 -0300

    feat: criação da estrutura do projeto e da classe DespesaFamiliar

---
+++ src/br/com/fiap/bean/DespesaFamiliar.java
@@ -0,0 +1,24 @@
+
+
+package br.com.fiap.bean;
+
+public class DespesaFamiliar {
+
+    // atributos
+    public double rendaFamiliar;
+    public int numeroDeMoradores;
+    public double gastoComLuz;
+    public double gastoComAgua;
+    public double gastoComInternet;
+    public double valorMensalidadeDaAcademia;
+
+    // métodos
+
+    public double calcularTotalDeDespesas() {
+        return 0;
+    }
+
+    public double calcularRendaFamiliarLiquida(){
+        return 0;
+    }
+}
